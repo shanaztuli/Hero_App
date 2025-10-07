1fab66eac5cf9cb7b626af721a3bfd101dc0a00e
shanaz tuli <shanazparbinloskortuli@gmail.com>
2025-10-08 00:05:22 +0100

    first commit

---
+++ src/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Root = () => {
+  return (
+    <div>
+      <h1>Heoolow worlsd</h1>
+    </div>
+  );
+};
+
+export default Root;
---
+++ src/Route/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [{}],
+  },
+]);
